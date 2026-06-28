8004fceaac8e16f6b9054909a55a83e287f45b10
Akhil Pundir <akhilpundir009@gmail.com>
2026-06-28 12:09:29 +0530

    second question

---
+++ Questions/main2.py
@@ -0,0 +1,20 @@
+# Find runner-up score
+import random
+arr=[]
+
+def random_score(n):
+  for i in range(n):
+    arr.append(random.randint(1,20))
+
+random_score(5)
+
+
+
+arr.sort()
+max_score = arr[-1]
+print(arr)
+for x in reversed(arr):
+  if x != max_score:
+    print(x)
+    break
+
